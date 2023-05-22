4b0a81c7db74331e6cf5b17053782df24d0ff461
David Oluwafemi <davidoluwafemi178@gmail.com>
2023-05-22 14:09:01 +0100

    Add tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      bid_price, ask_price = quote['top_bid']['price'], quote['top_ask']['price']
+      self.assertEqual(getDataPoint(quotes), (quote['stock'], bid_price, ask_price, (bid_price + ask_price)/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +18,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      bid_price, ask_price, *= getDataPoint(quote)
+      self.assertTrue(bid_price > ask_price)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRation_calculateRatio(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    stock1 = quotes[0]
+    bid_price1, ask_price1 = stock1['top_bid']['price'], stock1['top_ask']['price']
+    price1 = (bid_price1 + ask_price1) / 2
+    stock2 = quotes[1]
+    bid_price2, ask_price2 = stock2['top_bid']['price'], stock2['top_ask']['price']
+    price2 = (bid_price2 + ask_price2) / 2
+    self.assertEqual(price1/price2, getRatio(price1, price2))
+
 
 
 
